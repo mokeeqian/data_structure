a9ffc21333a0e6b7c821bf7eb76590e6a99d4960
mokeeqian <mokeeqian@qq.com>
2020-02-16 23:49:52 +0800

    add 直接插入排序注释

--- direct_insert_sort.c
+++ direct_insert_sort.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
+/**
+ * 升序的直接插入排序
+ */
 void directInsertSort(int *nums, int size) {
     int i, j;
     for ( i = 2; i < size; i++)
     {
-        nums[0] = nums[i];
-        for ( j = i-1; nums[0] < nums[j]; j-- )
+        // nums[0]作为前哨战，同时作为临时变量，可以避免不必要的if逻辑
+        nums[0] = nums[i];          // 将当前扫描到的值复制到前哨战
+        // 找元素待插位置。用当前元素(nums[0]值和他相等)不断和它前驱元素比较，如果比他前驱小，则他的前驱往后移动，
+        for ( j = i-1; nums[0] < nums[j]; j-- )     
         {
-            nums[j+1] = nums[j];
+            nums[j+1] = nums[j];            // 后移
         }
-        nums[j+1] = nums[0];
+        nums[j+1] = nums[0];                // 当前元素插入到待插的位置
     }
 }
 
+/**
+ * 测试
+ */
 int main(int argc, char const *argv[])
 {
     /* code */
