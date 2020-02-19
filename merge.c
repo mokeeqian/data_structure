14dad23c5b7ca0b49caf5df69f1d4761255154fc
mokeeqian <mokeeqian@qq.com>
2020-02-19 21:45:10 +0800

    mod 归并数组

--- merge.c
+++ merge.c
@@ -20,29 +20,30 @@ int main()
     return 0;
 }
 
+//按升序归并 a,b是目标数组， C是结果数组
 void merge(int a[], int b[], int c[]) {
-	int i = 0;
-	int j = 0;
-	int k = 0;
+	int i = 0;	// 遍历a数组
+	int j = 0;	// 遍历b数组
+	int k = 0;	// 访问c数组
 
 	// 两两比较
 	while( i <10 && j <10 ) {
 		if ( a[i] <= b[j] )
-			c[k++] = a[i++];
+			c[k++] = a[i++];	// 把较小的a[i]复制到c[k]中，i++, k++, 继续比较
 		else
-			c[k++] = b[j++];
+			c[k++] = b[j++];	// 
 	}
 
 	printf("i= %d\n", i);
 	printf("j= %d\n", j);
 
-	// 没比较完，做个处理，继续比较
+	// 没比较完，做个处理，继续比较 --> 这两个while 只会执行一个
 	while( i < 10 )
 		c[k++] = a[i++];
-
 	while( j < 10 )
 		c[k++] = b[j++];
-    for ( i = 0; i < 20; i++ )
+    
+	for ( i = 0; i < 20; i++ )
 		printf("%d ", c[i]);
 
     return;
